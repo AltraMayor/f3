2a6ac36ca7ebc51741f032926da49265e37c9ccc
Alexander A. Klimov <grandmaster@al2klimov.de>
2022-11-08 22:30:43 +0100

    f3read, f3write: chroot(2) to source/target dir if allowed

--- utils.c
+++ utils.c
@@ -29,6 +29,12 @@ void adjust_dev_path(const char **dev_path)
 		err(errno, "Can't change working directory to %s at %s()", *dev_path, __func__);
 	}
 
+	if (!chroot(*dev_path)) {
+		assert(!chdir("/"));
+	} else if (errno != EPERM) {
+		err(errno, "Can't change root directory to %s at %s()", *dev_path, __func__);
+	}
+
 	*dev_path = ".";
 }
 
